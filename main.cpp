cc730ce075d5c7e9d9fdc218d72e8c5f5812fa9b
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-11-20 22:57:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
          break;
       case 2: cout << a*b << endl;
          break;
-      default: cout << operazione non valida << endl;
+      default: cout << "operazione non valida" << endl;
    }
    return 0;
 }
